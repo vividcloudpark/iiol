9016dd5a189d308225a5fe4922f5fe1379aa770f
cloudpark <vividxenon@gmail.com>
2023-09-11 16:55:34 +0900

    기초 뼈대용 모델 생성

--- iiol/books/models.py
+++ iiol/books/models.py
@@ -1,17 +1,17 @@
 from django.db import models
+from iiol.models import BaseTimeModel
 
-# Create your models here.
 
-class Book(models.Model):
-    bookname = models.CharField(blank=False)
-    publication_date = models.CharField(max_length=4)
-    authors = models.CharField(blank=False)
-    publisher = models.CharField()
-    class_no = models.CharField()
-    class_nm = models.CharField()
-    publication_year: models.CharField()
+class Book(BaseTimeModel):
+    isbn13 = models.CharField(
+        blank=False, db_index=True, primary_key=True, max_length=13)
+    isbn = models.CharField(max_length=10)
+    bookname = models.CharField(blank=False, max_length=300)
+    publication_date = models.CharField(max_length=20)
+    authors = models.CharField(blank=False, max_length=100)
+    publisher = models.CharField(max_length=100)
+    class_no = models.CharField(max_length=100)
+    class_nm = models.CharField(max_length=100)
+    publication_year: models.CharField(max_length=20)
     bookImageURL = models.URLField()
-    isbn = models.CharField()
-    isbn13 = models.CharField(blank=False)
     description = models.TextField()
-
