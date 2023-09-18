e6c576d584a1b7ebda365d25220d079e1d0680c4
cloudpark <vividxenon@gmail.com>
2023-09-18 11:13:11 +0900

    Dockerized PostGreSQL로 DB 전환 완료, 설정법은 아래 velog 참고: https://velog.io/@cloud_park/Django%EC%97%90-Docker%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-PostgreSQL%EC%9D%84-%EC%97%B0%EB%8F%99%ED%95%B4%EB%B3%B4%EC%9E%90

--- iiol/iiol/settings/common.py
+++ iiol/iiol/settings/common.py
@@ -97,9 +97,17 @@ WSGI_APPLICATION = 'iiol.wsgi.application'
 
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'iiol',
+        'USER': 'django_user',
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
