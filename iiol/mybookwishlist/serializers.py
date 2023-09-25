675f65b367b7afcbcbe9b493c648598c39bafb62
cloudpark <vividxenon@gmail.com>
2023-09-25 20:38:47 +0900

    mybookwishlist init

--- iiol/mybookwishlist/serializers.py
+++ iiol/mybookwishlist/serializers.py
@@ -1,8 +1,18 @@
 from .models import MybookWishlist
+from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email']
 
 
 class MybookWishlistSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='user.username')
+
     class Meta:
         model = MybookWishlist
-        fields = ('when_it_put', 'memo')
+        fields = ('user',  'author_username', 'isbn13', 'when_it_put', 'memo',)
--- iiol/mybookwishlist/views.py
+++ iiol/mybookwishlist/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http import JsonResponse, StreamingHttpResponse
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import MybookWishlist
+from .serializers import MybookWishlistSerializer
+from rest_framework.response import Response  
+from django.conf import settings
 
-# Create your views here.
+
+User=settings.AUTH_USER_MODEL
+
+
+class MybookWishListViewSet(LoginRequiredMixin, APIView):
+    login_url = '/login/'
+    redirect_filed_name = 'redirect_to'
+    queryset = MybookWishlist.objects.all()
+    serializer_class = MybookWishlistSerializer
+
+    def get(self, request, format=None):
+        qs = MybookWishlist.objects.filter(user=request.user)
+        serializer = MybookWishlistSerializer(qs, many=True)
+        return Response(serializer.data)
