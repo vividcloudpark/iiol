0875fa0a39f391c76b07f84d7a4a0a3644c5bb92
cloudpark <vividxenon@gmail.com>
2023-10-11 10:35:49 +0900

    Mybookwishlist 폴더 추가, Account bio삭제 및 나의 지역/ 나의 도서관 추가

--- iiol/accounts/models.py
+++ iiol/accounts/models.py
@@ -3,34 +3,52 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.template.loader import render_to_string
 from django.core.validators import RegexValidator
+
 # Create your models here.
 
 
 class User(AbstractUser):
-    bio = models.TextField(blank=True)
-    phone_number = models.CharField(
-        validators=[RegexValidator("^010-?[1-9]\d{3}-?\d{4}$")],
-        max_length=14,
-        default='000-0000-0000')
+    # bio = models.TextField(blank=True)
+    # phone_number = models.CharField(
+    #     validators=[RegexValidator("^010-?[1-9]\d{3}-?\d{4}$")],
+    #     max_length=14,
+    #     default='000-0000-0000')
 
-    # my_region = models.ForeignKey(
-    #     "library.SmallRegion", on_delete=models.CASCADE)
-    # my_library = models.ForeignKey(
-    #     "library.Library", on_delete=models.CASCADE)
+    my_region = models.ForeignKey(
+        "library.SmallRegion",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        default=None,
+        help_text="바코드 검색시 기본으로 사용할 지역",
+    )
+    my_library = models.ForeignKey(
+        "library.Library",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        default=None,
+        help_text="선호하는 도서관",
+    )
 
     @property
     def user_name(self):
         return f"{self.first_name} {self.last_name}"
 
     def send_welcome_email(self):
-        subject = render_to_string("accounts/welcome_email_subject.txt", {
-            "user": self,
-        })
+        subject = render_to_string(
+            "accounts/welcome_email_subject.txt",
+            {
+                "user": self,
+            },
+        )
 
-        content = render_to_string("accounts/welcome_email_content.txt", {
-            "user": self,
-        })
+        content = render_to_string(
+            "accounts/welcome_email_content.txt",
+            {
+                "user": self,
+            },
+        )
 
         sender_email = settings.WELCOME_EMAIL_SENDER
-        send_mail(subject, content,
-                  sender_email[self.email], fail_silently=False)
+        send_mail(subject, content, sender_email[self.email], fail_silently=False)
--- iiol/mybookwishlist/models.py
+++ iiol/mybookwishlist/models.py
@@ -3,18 +3,33 @@ from iiol.models import BaseTimeModel
 
 
 class MybookWishlist(BaseTimeModel):
-    user = models.ForeignKey(
-        "accounts.User", on_delete=models.CASCADE)
-    isbn13 = models.ForeignKey(
-        "books.Book", on_delete=models.CASCADE)
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    isbn13 = models.ForeignKey("books.Book", on_delete=models.CASCADE)
     input_context = models.CharField(max_length=100, blank=True)
     memo = models.TextField(blank=True)
     readYn = models.BooleanField(default=False)
     readDate = models.DateField(
-        blank=True, null=True, auto_now=False, auto_now_add=False)
+        blank=True, null=True, auto_now=False, auto_now_add=False
+    )
+    groupname = models.ForeignKey(
+        "mybookwishlist.MybookWishlistGroup", on_delete=models.PROTECT
+    )
 
     class Meta:
-        unique_together = ('user', 'isbn13')
+        unique_together = ("user", "isbn13")
 
     def __str__(self):
-        return f'{self.user}/{self.isbn13.isbn13}  | {self.isbn13.bookname} | {self.memo}'
+        return (
+            f"{self.user}/{self.isbn13.isbn13}  | {self.isbn13.bookname} | {self.memo}"
+        )
+
+
+class MybookWishlistGroup(BaseTimeModel):
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    name = models.CharField(max_length=10, null=False, blank=False, default="분류없음")
+
+    class Meta:
+        unique_together = ("user", "name")
+
+    def __str__(self):
+        return f"{self.user}의 [{self.name}]"
